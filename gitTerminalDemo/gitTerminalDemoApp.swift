1da30d98f8512d950c93199b53afbc89a787574b
Kodi Maberek <kodi.maberek23@gmail.com>
2023-09-14 07:36:30 -0400

    Initial Commit

---
+++ gitTerminalDemo/gitTerminalDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTerminalDemoApp.swift
+//  gitTerminalDemo
+//
+//  Created by Rodion Akhmedov on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct gitTerminalDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
